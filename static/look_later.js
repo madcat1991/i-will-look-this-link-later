bff36a89d51365791a46d96f9777e91541df120f
Tural Gurbanov <madcat1991@gmail.com>
2013-03-04 01:39:18 +0400

    JS for the page, and page

--- static/look_later.js
+++ static/look_later.js
@@ -1,7 +1,38 @@
-/**
- * Created with PyCharm.
- * User: tural
- * Date: 03.03.13
- * Time: 1:21
- * To change this template use File | Settings | File Templates.
- */
+$(document).ready(function(){
+    var request;
+
+    $("#push_form").submit(function(event){
+        // abort any pending request
+        if (request) {
+            request.abort();
+        }
+
+        // fire off the request to /form.php
+        var request = $.ajax({
+            url: "/push",
+            type: "post",
+            data: $(this).serialize()
+        });
+
+        // callback handler that will be called on success
+        request.done(function (response, textStatus, jqXHR){
+            // log a message to the console
+            if(response["success"] === "ok"){
+                alert("Hooray, it worked!");
+            } else {
+                alert(response["message"]);
+            }
+        });
+
+        // callback handler that will be called on failure
+        request.fail(function (jqXHR, textStatus, errorThrown){
+            // log the error to the console
+            console.error(
+                "The following error occured: "+
+                    textStatus, errorThrown
+            );
+        });
+        // prevent default posting of form
+        event.preventDefault();
+    });
+});
