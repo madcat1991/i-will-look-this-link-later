dbebd982506c71ac28023d6c8962e422b1255454
Tural Gurbanov <madcat1991@gmail.com>
2013-03-10 02:06:51 +0400

    main page style and transparent labels

--- static/js/look_later.js
+++ static/js/look_later.js
@@ -35,4 +35,34 @@ $(document).ready(function(){
         // prevent default posting of form
         event.preventDefault();
     });
+
+    $('.url-label, .pass-label')
+        .animate({ opacity: "0.4" })
+        .click(function() {
+            var thisFor	= $(this).attr('for');
+            $('.'+thisFor).focus();
+        });
+
+    $('#url').focus(function() {
+        $('.url-label').animate({ opacity: "0" }, "fast");
+        if($(this).val() == "url")
+            $(this).val() == "";
+    }).blur(function() {
+        if($(this).val() == "") {
+            $(this).val() == "url";
+            $('.username-label').animate({ opacity: "0.4" }, "fast");
+        }
+    });
+
+    $('#pass').focus(function() {
+        $('.pass-label').animate({ opacity: "0" }, "fast");
+        if($(this).val() == "password") {
+            $(this).val() == "";
+        }
+    }).blur(function() {
+        if($(this).val() == "") {
+            $(this).val() == "password";
+            $('.pass-label').animate({ opacity: "0.4" }, "fast");
+        }
+    });
 });
