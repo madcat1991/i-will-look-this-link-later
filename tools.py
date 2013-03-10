3bc67f7715d7578d52fce259bef19ad25448ab37
Tural Gurbanov <madcat1991@gmail.com>
2013-03-10 14:37:41 +0400

    url checker

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@
 import re
 
 
-def simple_url_checker(input_str, pattern="^((https?|ftp)://)?\S+?\.\S+?(/\S+)?$"):
+def simple_url_checker(input_str, pattern="^((https?|ftp)://)?\S+?(/\S+)?$"):
     """Checks that inputted string is url
     """
     if re.search(pattern, input_str):
